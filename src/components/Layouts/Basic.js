d656ead7cdd4805f73e8e047298d2a5c9b974d03
Alan Alvarenga <Alan.AlvarengaMejia@telus.com>
2020-06-28 17:25:45 -0600

    tailwind setup completed

--- src/components/Layouts/Basic.js
+++ src/components/Layouts/Basic.js
@@ -1,16 +1,6 @@
-
 import React from 'react';
 import { Layout, Menu } from 'antd';
-// import {
-//   AppstoreOutlined,
-//   BarChartOutlined,
-//   CloudOutlined,
-//   ShopOutlined,
-//   TeamOutlined,
-//   UserOutlined,
-//   UploadOutlined,
-//   VideoCameraOutlined,
-// } from '@ant-design/icons';
+
 
 const { Header, Content, Footer, Sider } = Layout;
 const Basic = (props) => (
@@ -23,28 +13,23 @@ const Basic = (props) => (
         left: 0,
       }}
     >
-      <div className="logo" />
+      <div className="subpixel-antialiased m-3 text-lg text-white text-semibold" >
+        <p>Ocelot Deploy Tool</p>
+      </div>
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
-        <Menu.Item key="1">
-          <span className="nav-text">TFS Deploy</span>
-        </Menu.Item>
-        {/* <Menu.Item key="2">
-          <UserOutlined />
-          <span className="nav-text">nav 1</span>
+        <Menu.Item key="2">
+          <span className="nav-text">FOMC Instances</span>
         </Menu.Item>
         <Menu.Item key="3">
-          <VideoCameraOutlined />
-          <span className="nav-text">nav 2</span>
+          <span className="nav-text">New Instance</span>
         </Menu.Item>
-        <Menu.Item key="4">
-          <UploadOutlined />
-          <span className="nav-text">nav 3</span>
-        </Menu.Item> */}
       </Menu>
+
     </Sider>
     <Layout className="site-layout" style={{ marginLeft: 200 }}>
       <Header className="site-layout-background" style={{ padding: 0 }} />
       <Content className="site-content">
+
         <div className="site-layout-background" style={{ padding: 24}}>
           {props.children}
         </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.css';
+import './tailwind.generated.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
